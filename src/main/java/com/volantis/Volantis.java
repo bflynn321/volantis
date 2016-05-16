296b2c2cb94fef919870769d67539a2a0c2236f2
Tormund Giantsbane <bflynn9@fau.edu>
2016-05-16 17:26:44 -0400

    Update Volantis.java
    Created Dragon class.

--- src/main/java/com/volantis/Volantis.java
+++ src/main/java/com/volantis/Volantis.java
@@ -5,5 +5,32 @@ public class Volantis
 	public static void main(String[] args)
 	{
 		System.out.println("Panda panda panda panda");
+		Dragon Drogon = new Dragon("Drogon");
+		Dragon Rhaegal = new Dragon("Rhaegal");
+		Dragon Viserion = new Dragon("Viserion");
 	}
+}
+public class Dragon
+{
+	String color;
+	String size;
+	String name;
+	int health = 100;
+	public Dragon(String n)
+	{
+		name = n;
+	}
+	void breatheFire()
+	{
+		//int damage = rand.nextInt((max - min) + 1) + min;
+	}
+	void fly()
+	{
+		System.out.println(name + " flaps his enormous wings and takes flight!");
+	}
+	void sleeping()
+	{
+		System.out.println(name + " is sleeping...you are never supposed to wake a sleeping dragon..it is known, IT IS KNOWN");
+	}
+
 }
